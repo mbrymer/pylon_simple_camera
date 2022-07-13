dacc589a7eaf698f74d5a196759d89529e384c3c
Matt Brymer <brymer.matt@gmail.com>
2022-07-12 22:07:35 -0400

    Split out parameters into config yaml

--- src/pylon_simple_camera_node.cpp
+++ src/pylon_simple_camera_node.cpp
@@ -20,53 +20,9 @@ Nearly zero input/error checking, use at your own risk
 // std lib includes
 #include <string.h>
 
-// Parameters
-std::string image_topic = "/basler_camera/image_raw";
-std::string camera_info_topic = "/basler_camera/camera_info";
-std::string camera_topic = "/basler_camera/image_raw";
-int grab_timeout = 5000; // ms
-double run_rate = 20.; // Hz
-
-// Hard coded calibration parameters (move these to .yaml at some point)
-// double K[] = {1775.6863472419932, 0, 828.2282538567714,
-//                 0, 1776.8185379727229, 529.7529387767097,
-//                 0,0,1}; // Intrinsics
-
-// double K[] = {1775.6863472419932, 0, 988,
-//                0, 1776.8185379727229, 568,
-//                0,0,1}; // Intrinsics
-
-// double K[] = {1775.6863472419932, 0, 1324,
-//                 0, 1776.8185379727229, 1001,
-//                 0,0,1}; // Intrinsics
-
-double K[] = {443.75, 0, 331,
-                0, 443.75, 250.25,
-                0,0,1}; // Intrinsics
-
-double D[] = {-0.3104266996245532, 0.13583691372392823, -0.0006515077009149717, 0.0002375047372993956,0}; // Distortion coefficients
-// int image_width = 1600;
-// int image_height = 1000;
-// int image_offsetX = 496;
-// int image_offsetY = 472;
-
-// int image_width = 1920;
-// int image_height = 1080;
-// int image_offsetX = 752;
-// int image_offsetY = 432;
-
-// int image_width = 2592;
-// int image_height = 1944;
-// int image_offsetX = 0;
-// int image_offsetY = 0;
-
-int image_width = 640;
-int image_height = 480;
-int image_offsetX = 0;
-int image_offsetY = 0;
+using namespace Pylon;
 
 // Declarations
-// bool grab_image(sensor_msgs::Image& image_msg);
 bool genAPI2Ros(const std::string& gen_api_enc, std::string& ros_enc);
 
 int main (int argc, char **argv)
@@ -75,48 +31,62 @@ int main (int argc, char **argv)
     ros::init(argc, argv, "pylon_simple_camera");
     ros::NodeHandle node_pylon_camera;
 
+    // Grab parameters
+    std::string camera_info_topic;
+    std::string camera_topic;
+    int grab_timeout; // ms
+    double run_rate; // Hz
+
+    std::vector<double> K; // Intrinsics
+    std::vector<double> D; // Distortion coefficients
+
+    int image_width;
+    int image_height;
+    int image_offsetX;
+    int image_offsetY;
+
+    bool use_scaling;
+
+    node_pylon_camera.param<std::string>("camera_topic",camera_topic,"/basler_camera/image_raw");
+    node_pylon_camera.param<std::string>("camera_info_topic",camera_info_topic,"/basler_camera/camera_info");
+
+    node_pylon_camera.param<double>("run_rate",run_rate,20.0);
+    node_pylon_camera.param<int>("grab_timeout",grab_timeout,5000);
+    node_pylon_camera.param<int>("image_width",image_width,640);
+    node_pylon_camera.param<int>("image_height",image_height,480);
+    node_pylon_camera.param<int>("image_offsetX",image_offsetX,0);
+    node_pylon_camera.param<int>("image_offsetY",image_offsetY,0);
+
+    node_pylon_camera.param<bool>("use_scaling",use_scaling,true);
+
+    node_pylon_camera.getParam("K",K);
+    node_pylon_camera.getParam("D",D);
+
     // Run Rate
     ros::Rate node_rate(run_rate);
 
     // Define publishers
-    // ros::Publisher image_pub = node_pylon_camera.advertise<sensor_msgs::Image>(image_topic,1);
-    // ros::Publisher camera_info_pub = node_pylon_camera.advertise<sensor_msgs::CameraInfo>(camera_info_topic,1);
-
     image_transport::ImageTransport it_pylon_camera(node_pylon_camera);
     image_transport::CameraPublisher camera_pub = it_pylon_camera.advertiseCamera(camera_topic,1);
 
     // Start up Pylon and camera
     Pylon::PylonAutoInitTerm pylon_autoterm;
     Pylon::CBaslerUniversalInstantCamera myCamera(Pylon::CTlFactory::GetInstance().CreateFirstDevice() );
+
     // Clear the default camera configuration, use values set on camera itself
     myCamera.RegisterConfiguration( (Pylon::CConfigurationEventHandler*) NULL, Pylon::RegistrationMode_ReplaceAll, Pylon::Cleanup_None);
     myCamera.Open();
 
-    // Decrease ROI. Hard coded for now just to figure out how this works
+    // Set ROI
     myCamera.Height.SetValue(image_height);
     myCamera.Width.SetValue(image_width);
     myCamera.OffsetX.SetValue(image_offsetX);
     myCamera.OffsetY.SetValue(image_offsetY);
 
-// Node Name:
-   
-// BslScalingEnable
-// Interface Type:
-   
-// IBoolean
-
-    // myCamera.BslScalingEnable.SetValue(true);
-
-    // Pylon::CInstantCamera mynewCamera(Pylon::CTlFactory::GetInstance().CreateFirstDevice() );
-    // mynewCamera.BslScalingEnable.
-
-    using namespace Pylon;
-
+    // Enable downsampling
     GenApi::INodeMap& nodemap = myCamera.GetNodeMap();
-
-    // Pylon::GenApi::INodeMap& nodemap = myCamera.GetNodeMap();
-    CBooleanParameter(nodemap, "BslScalingEnable").SetValue(true);
-
+    CBooleanParameter(nodemap, "BslScalingEnable").SetValue(use_scaling);
+    
     myCamera.StartGrabbing(Pylon::GrabStrategy_LatestImageOnly);
 
     // Check pixel format is valid
@@ -137,9 +107,6 @@ int main (int argc, char **argv)
         // Initialize image message object
         sensor_msgs::Image image_msg;
         sensor_msgs::CameraInfo camera_info_msg;
-
-        // Grab image and put in .data field
-        // bool grab_successful = grab_image(image_msg);
 
         // Create pointer to store captured image and grab image
         Pylon::CBaslerUniversalGrabResultPtr ptrGrabResult;
@@ -175,23 +142,20 @@ int main (int argc, char **argv)
             camera_info_msg.header.stamp = stamp_now;
             camera_info_msg.height = image_height;
             camera_info_msg.width = image_width;
-            camera_info_msg.K = {443.75, 0, 331,
-                                0, 443.75, 250.25,
-                                0,0,1}; // Intrinsics
-            camera_info_msg.P = {443.75, 0, 331,0,
-                                0, 443.75, 250.25,0,
-                                0,0,1,0}; // Intrinsics
+            camera_info_msg.K = {K[0], K[1], K[2],
+                                K[3], K[4], K[5],
+                                K[6], K[7], K[8]}; // Intrinsics
+            camera_info_msg.P = {K[0], K[1], K[2],0,
+                                K[3], K[4], K[5],0,
+                                K[6], K[7], K[8],0}; 
             camera_info_msg.R = {1,0,0,
                                  0,1,0,
                                  0,0,1}; // Rectification
-            camera_info_msg.D = {-0.3104266996245532, 0.13583691372392823,
-                                -0.0006515077009149717, 0.0002375047372993956, 0}; // Distortion coefficients
-            //{-0.3104266996245532, 0.13583691372392823, -0.0006515077009149717, 0.0002375047372993956,0}; // Distortion coefficients
+            camera_info_msg.D = D; // Distortion coefficients
+            
             camera_info_msg.distortion_model = "plumb_bob";
 
             // Publish image
-            // image_pub.publish(image_msg);
-            // camera_info_pub.publish(camera_info_msg);
             camera_pub.publish(image_msg,camera_info_msg);
         }
         else
